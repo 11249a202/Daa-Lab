0323ab9c2a9fefd5f8b369dac4bc64aa97dd593c
11249a202 <11249a202@kanchiuniv.ac.in>
2026-04-26 12:10:50 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,32 @@
+Aim:
+To write a C program that calculates the n-th Fibonacci number using recursion.
+
+Program:
+#include <stdio.h>
+// Function to calculate Fibonacci number using recursion
+int fib(int n){  
+    // Base case: if n is 0, return 0
+    if (n == 0){
+        return 0;
+    }
+    // Base case: if n is 1, return 1
+    if (n == 1) {
+        return 1;
+    }
+    // Recursive case: sum of previous two Fibonacci numbers
+    return fib(n-1) + fib(n-2); 
+}  
+int main() {
+    int n;
+    // Prompt user to enter number
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    // Check for invalid input (negative number)
+    if (n < 0){
+        printf("Invalid number");
+        return 0;
+    }
+    // Display the nth Fibonacci term
+    printf("Fibonacci term is: %d", fib(n));
+    return 0;
+}
